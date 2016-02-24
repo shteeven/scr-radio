8ef5683dd9f80a812d8378595d54bbdfcddb4583
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-02-24 12:42:01 -0700

    install ng-material and dependencies

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,9 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/angular-material/angular-material.css',
+        'public/lib/angular-material/angular-material.layout.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -16,7 +18,9 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/angular-material/angular-material.js',
+        'public/lib/angular-aria/angular-aria.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },

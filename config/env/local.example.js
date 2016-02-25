9fe23ede99392cee07f0972972ab242c068cf198
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-02-25 12:05:57 -0700

    Set local.example to comment out all vars by default, allowing the developer to choose what vars to insert with local.

--- config/env/local.example.js
+++ config/env/local.example.js
@@ -4,60 +4,41 @@
 // will not get commited and pushed to remote repositories.
 // Use it for your API keys, passwords, etc.
 
-/* For example:
-
-module.exports = {
-  db: {
-    uri: 'mongodb://localhost/local-dev',
-    options: {
-      user: '',
-      pass: ''
-    }
-  },
-  sessionSecret: process.env.SESSION_SECRET || 'youshouldchangethistosomethingsecret',
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/facebook/callback'
-  }
-};
-*/
-
-
 // SCRadio implementation of local
 module.exports = {
   // Start environment to run app; I may leave this as the only environment variable to be set at run time
   //NODE_ENV: '',
 
   // Default config file vars
-  GOOGLE_ANALYTICS_TRACKING_ID: '',
+  //GOOGLE_ANALYTICS_TRACKING_ID: '',
 
   // Variables for the Dev or Production config files
-  MONGO_DB: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/scradio',
-  MONGO_USER: '',
-  MONGO_PWD: '',
-  SESSION_SECRET: '',
-  MONGOLABURI: '',
-  PORT: '',
+  //MONGO_DB: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/scradio',
+  //MONGO_USER: '',
+  //MONGO_PWD: '',
+  //SESSION_SECRET: '',
+  //MONGOLABURI: '',
+  //PORT: '',
+
   // vendor specific
-  FACEBOOK_ID: '',
-  FACEBOOK_SECRET: '',
-  TWITTER_KEY: '',
-  TWITTER_SECRET: '',
-  GOOGLE_ID: '',
-  GOOGLE_SECRET: '',
-  LINKEDIN_ID: '',
-  LINKEDIN_SECRET: '',
-  GITHUB_ID: '',
-  GITHUB_SECRET: '',
-  PAYPAL_ID: '',
-  PAYPAL_SECRET: '',
-  MAILER_FROM: '',
-  MAILER_SERVICE_PROVIDER: '',
-  MAILER_EMAIL_ID: '',
-  MAILER_PASSWORD: '',
+  //FACEBOOK_ID: '',
+  //FACEBOOK_SECRET: '',
+  //TWITTER_KEY: '',
+  //TWITTER_SECRET: '',
+  //GOOGLE_ID: '',
+  //GOOGLE_SECRET: '',
+  //LINKEDIN_ID: '',
+  //LINKEDIN_SECRET: '',
+  //GITHUB_ID: '',
+  //GITHUB_SECRET: '',
+  //PAYPAL_ID: '',
+  //PAYPAL_SECRET: '',
+  //MAILER_FROM: '',
+  //MAILER_SERVICE_PROVIDER: '',
+  //MAILER_EMAIL_ID: '',
+  //MAILER_PASSWORD: '',
 
   // Where log files are stored
-  LOG_DIR_PATH: ''
+  //LOG_DIR_PATH: ''
 };
 

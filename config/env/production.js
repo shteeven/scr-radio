bab51d5447b05837456bc489438b36107f6eb69e
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-02-25 23:14:26 -0700

    run build

--- config/env/production.js
+++ config/env/production.js
@@ -11,8 +11,8 @@ module.exports = {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/scradio',
     options: {
       // TODO: undo the hardcoding on these.
-      user: local.MONGO_USER || '',
-      pass: local.MONGO_PWD || ''
+      user: '',
+      pass: ''
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false

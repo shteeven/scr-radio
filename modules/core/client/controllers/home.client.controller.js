9734cab0acadc7390ca54761fb31ede8748dff11
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-06-21 19:27:45 -0600

    build

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,21 +1,17 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Episodes', 'Specials', 'Regulars', 'Things', '$http',
-  function ($scope, Authentication, Episodes, Specials, Regulars, Things, $http) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Contents',
+  function ($scope, Authentication, Contents) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
     $scope.tile_limit = 12;
 
-    // $scope.episode_tiles = Episodes.query();
-    // $scope.special_tiles = Specials.query();
 
-    $scope.tiles = Things.query({ category: 'tiles' });
+    $scope.tiles = Contents.query({ featured: 'tiles' }, function (data) {
+      console.log(data);
+    });
 
-    $scope.slides = Things.query({ category: 'carousel' });
-
-    // $scope.tiles = Things.query({type: 'tiles'}, function (data) {
-    //   console.log(data);
-    // });
+    $scope.slides = Contents.query({ featured: 'carousel' });
 
   }
 ]);

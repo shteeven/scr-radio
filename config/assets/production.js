dcc33c01318d7dee8fd8d67f7ccfa6fa4f70b457
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-06-21 21:29:18 -0600

    build

--- config/assets/production.js
+++ config/assets/production.js
@@ -6,6 +6,10 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/angular-material/angular-material.css',
+        'public/lib/angular-material/angular-material.layout.css',
+        'public/lib/font-awesome/css/font-awesome.css',
+        'public/lib/sc-date-time/dist/sc-date-time.css'
       ],
       js: [
         'public/lib/angular/angular.min.js',
@@ -13,10 +17,12 @@ module.exports = {
         'public/lib/angular-animate/angular-animate.min.js',
         'public/lib/angular-messages/angular-messages.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/angular-material/angular-material.js',
+        'public/lib/angular-aria/angular-aria.js',
+        'public/lib/moment/moment.js',
+        'public/lib/sc-date-time/dist/sc-date-time.js'
       ]
     },
     css: 'public/dist/application.min.css',

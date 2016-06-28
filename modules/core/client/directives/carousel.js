8fb22d1246894977ea974630560aa26d4deec1c2
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-06-27 21:52:01 -0600

    build

--- modules/core/client/directives/carousel.js
+++ modules/core/client/directives/carousel.js
@@ -23,7 +23,7 @@ app.directive('scrCarousel', function($rootScope, $http, $interval, $state) {
 
       };
 
-      var rotationDelay = 6800000;
+      var rotationDelay = 6800;
 
       $scope.isCurrentSlideIndex = function (index) {
         return $scope.currentIndex === index;
